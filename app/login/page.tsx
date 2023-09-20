34549dfe82d02d2c52a5abd051501b57d5553782
Subhan Zafar Ansari <contact.subhanzafar@gmail.com>
2023-09-20 03:54:05 -0700

    Setup the sign in/sign up switch

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,18 @@
 "use client";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Input from "@/components/Input";
 
 function page() {
   const [email, setEmail] = useState("");
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+
+  const [varient, setVarient] = useState("login");
+  const toggleVarient = useCallback(() => {
+    setVarient((currentVarient) =>
+      currentVarient === "login" ? "register" : "login"
+    );
+  }, []);
 
   return (
     // Setting up the background
@@ -24,18 +31,22 @@ function page() {
         {/* The sign in box */}
         <div className=" flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-centre mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className=" text-white text-4xl mb-8 font-semibold">Sign in</h2>
+            <h2 className=" text-white text-4xl mb-8 font-semibold">
+              {varient === "login" ? "Sign in" : "Register"}
+            </h2>
             <div className="flex flex-col gap-4">
               {/* User Name Input */}
-              <Input
-                Label="User Name"
-                onChange={(ev: any) => {
-                  setUserName(ev.target.value);
-                }}
-                id="userName"
-                type="userName"
-                value={userName}
-              ></Input>
+              {varient === "register" && (
+                <Input
+                  Label="User Name"
+                  onChange={(ev: any) => {
+                    setUserName(ev.target.value);
+                  }}
+                  id="userName"
+                  type="userName"
+                  value={userName}
+                ></Input>
+              )}
               {/* E-mail input */}
               <Input
                 Label="E-mail"
@@ -56,10 +67,17 @@ function page() {
                 type="password"
                 value={password}
               ></Input>
-              <button className="bg-red-600 py-3 text-white rounded-md w-full hover:bg-red-700 transition">Login</button>
-              <p className="text-neutral-500 mt-12">
-                First time usning Netflix?
-                <span className="text-white ml-1 hover:underline cursor-pointer">Create an Account</span>
+              <button className="bg-red-600 py-3 text-white rounded-md w-full hover:bg-red-700 transition">
+                {varient === 'login'? 'Sign in': 'Sign up'}
+              </button>
+              <p className="text-neutral-500 mt-8">
+                {varient==='login' ? "First time usning Netflix?" : "Already have an account"}
+                <span
+                  onClick={toggleVarient}
+                  className="text-white ml-1 hover:underline cursor-pointer"
+                >
+                  {varient === "login"? 'Create an account': 'Login to existing account'}
+                </span>
               </p>
             </div>
           </div>
